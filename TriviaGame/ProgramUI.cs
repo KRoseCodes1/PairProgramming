472443c39569c791a28ba1006b6fc80f96e8001c
Kacie Kasner <krosecodes@gmail.com>
2021-07-22 21:09:44 -0400

    Updated UI, added prettification

--- TriviaGame/ProgramUI.cs
+++ TriviaGame/ProgramUI.cs
@@ -20,8 +20,11 @@ namespace TriviaGame
             while (keepRunning)
             {
                 Console.Clear();
-                Console.WriteLine("Welcome to our Trivia Game!\n" +
-                "Please choose an option:\n" +
+                Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "||                        Welcome to our TRIVIA GAME!!!!                        ||\n" +
+                "||                         Please choose an option:                             ||\n" +
+                "----------------------------------------------------------------------------------\n" +
+                "----------------------------------------------------------------------------------\n" +
                 "1. Start New Game\n" +
                 "2. View Scoreboard\n" +
                 "3. Exit Program");
@@ -32,14 +35,16 @@ namespace TriviaGame
                 {
                     case "1":
                         Player playerOne = PlayGame();
-                        scoreboard[1] = playerOne;
+                        Array.Resize(ref scoreboard, scoreboard.Length + 1);
+                        scoreboard[scoreboard.Length - 1] = playerOne;
                         break;
                     case "2":
                         DisplayScores(scoreboard);
                         break;
                     case "3":
                         keepRunning = false;
-                        Console.WriteLine("Goodbye!");
+                        Console.WriteLine("Thanks for playing! Goodbye! (Press any key to continue...");
+                        Console.ReadLine();
                         break;
                     default:
                         Console.WriteLine("Invalid response.");
@@ -65,9 +70,14 @@ namespace TriviaGame
             score = QuestionNine(score);
             score = QuestionTen(score);
             Console.Clear();
-            Console.WriteLine($"Congratulations! You scored {score} points so far!\n" +
-                $"Are you ready for LEVEL TWO?\n" +
-                $"In this round, every question is worth 2 points.");
+            Console.WriteLine($"----------------------------------------------------------------------------------\n" +
+                "----------------------------------------------------------------------------------\n" +
+                $"||                  Congratulations! You scored {score} points so far!                ||\n" +
+                "||                         Are you ready for LEVEL TWO?                          ||\n" +
+                "||                  In this round, every question is worth 2 points.             ||\n" +
+                "||                        (Press any key to continue...)                         ||" +
+                "----------------------------------------------------------------------------------\n" +
+                "----------------------------------------------------------------------------------");
             Console.ReadLine();
             score = QuestionEleven(score);
             score = QuestionTwelve(score);
@@ -75,7 +85,21 @@ namespace TriviaGame
             score = QuestionFourteen(score);
             score = QuestionFifteen(score);
             Console.Clear();
-            Console.WriteLine($"GAME OVER. You score {score} points!");
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "----------------------------------------------------------------------------------\n" +
+                $"||                        GAME OVER. You scored {score} points!                       ||\n" +
+                "----------------------------------------------------------------------------------\n" +
+                "----------------------------------------------------------------------------------\n" +
+                "             ___________\n" +
+                "            '._==_==_=_.'\n" +
+                "            .-\\:      /-.\n" +
+                "           | (|:.     |) |\n" +
+                "            '-|:.     |-'\n" +
+                "              \\::.    /\n" +
+                "               '::. .'\n" +
+                "                 ) ( \n" +
+                "               _.' '._ \n" +
+                "              `'''''''''");
             Console.ReadLine();
             one.highScore = score;
             return one;
@@ -84,7 +108,9 @@ namespace TriviaGame
         private Player CreateNewPlayer()
         {
             Player one = new Player();
-            Console.WriteLine("Please enter your name:");
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Please enter your name:\n" +
+                "----------------------------------------------------------------------------------");
             string name = Console.ReadLine();
             one.highScore = 0;
             one.name = name;
@@ -93,26 +119,34 @@ namespace TriviaGame
         private void DisplayScores(Player[] scores)
         {
             Console.Clear();
-            var orderedScores = scores.OrderByDescending(x => x.highScore);
-            Console.WriteLine("HIGH SCORES!:");
-            foreach(Player player in orderedScores)
+            var highScores = scores.OrderByDescending(x => x.highScore);
+            Console.WriteLine("------------------------------------------------------\n" +
+                "||             SCOREBOARD                            ||\n" +
+                "------------------------------------------------------\n" +
+                "------------------------------------------------------\n" +
+                "||  SCORE             ||          NAME               ||\n" +
+                "------------------------------------------------------");
+            foreach(Player player in highScores)
             {
                 if (player != null) 
                 {
-                    Console.WriteLine($"User:{player.name}  |  Score: {player.highScore}\n");
+                    Console.WriteLine($"  {player.highScore}              |          {player.name}    \n");
                 }
             }
+            Console.WriteLine("------------------------------------------------------");
             Console.ReadLine();
         }
         // Questions:
         private int QuestionOne(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question one: How many chambers does an Alligator's heart have?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question ONE: How many chambers does an Alligator's heart have?\n" +
                 "A. 4\n" +
                 "B. 5\n" +
                 "C. 3\n" +
-                "D. 2");
+                "D. 2\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -142,11 +176,13 @@ namespace TriviaGame
         private int QuestionTwo(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question two: What is the largest planet in our solar system?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question TWO: What is the largest planet in our solar system?\n" +
                 "A. Earth\n" +
                 "B. Mercury\n" +
                 "C. Jupiter\n" +
-                "D. Venus");
+                "D. Venus\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -176,11 +212,13 @@ namespace TriviaGame
         private int QuestionThree(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question three: What is the most widely used language in the world?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question THREE: What is the most widely used language in the world?\n" +
                 "A. English\n" +
                 "B. Mandarin\n" +
                 "C. Arabic\n" +
-                "D. Spanish");
+                "D. Spanish\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -210,11 +248,13 @@ namespace TriviaGame
         private int QuestionFour(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question four: What is the most commonly used letter of the alphabet?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question FOUR: What is the most commonly used letter of the alphabet?\n" +
                 "A. A\n" +
                 "B. S\n" +
                 "C. R\n" +
-                "D. E");
+                "D. E\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -244,11 +284,13 @@ namespace TriviaGame
         private int QuestionFive(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question five: How many hearts does an octopus have?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question FIVE: How many hearts does an octopus have?\n" +
                 "A. Five\n" +
                 "B. Eight\n" +
                 "C. One\n" +
-                "D. Three");
+                "D. Three\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -278,11 +320,13 @@ namespace TriviaGame
         private int QuestionSix(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question six: What is the largest mammal in the world?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question SIX: What is the largest mammal in the world?\n" +
                 "A. Whale\n" +
                 "B. Elephant\n" +
                 "C. Moose\n" +
-                "D. Hippopatamus");
+                "D. Hippopatamus\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -312,11 +356,13 @@ namespace TriviaGame
         private int QuestionSeven(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question seven: What is a group of hippos called?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question SEVEN: What is a group of hippos called?\n" +
                 "A. Pack \n" +
                 "B. Pod \n" +
                 "C. Bloat \n" +
-                "D. Family");
+                "D. Family\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -346,11 +392,27 @@ namespace TriviaGame
         private int QuestionEight(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question eight: What is Darth Vader's real name?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question EIGHT: What is Darth Vader's real name?\n" +
                 "A. Obi Wan Kenobi\n" +
                 "B. Luke Skywalker\n" +
                 "C. Anakin Skywalker \n" +
-                "D. Emperor Palpatine");
+                "D. Emperor Palpatine\n" +
+                "----------------------------------------------------------------------------------\n" +
+                "                       .-.\n" +
+                "                      |_:_|\n" +
+                "                     /(_Y_)\\ \n" +
+                ".                   ( \\/M\\/ )\n" +
+                " '.               _.'-/'-'\\-'._\n" +
+                "   ':           _/.--'[[[[]'--.\\_\n" +
+                "     ':        /_'  : |::' | :  '.\\ \n" +
+                "       ':     //   ./ |oUU| \\.'  :\\ \n" +
+                "         ':  _:'..' \\_|___|_/ :   :| \n" +
+                "          ':.  .'  |_[___]_|  :.':\\ \n" +
+                "            [::\\ |  :  | |  :   ; : \\ \n" +
+                "             '-'   \\/'.| |.' \\  .;.' | \n" +
+                "             |\\_    \\  '-'   :       | \n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -380,11 +442,13 @@ namespace TriviaGame
         private int QuestionNine(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question nine: Approx. how many feet are in a mile?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question NINE: Approx. how many feet are in a mile?\n" +
                 "A. 3,820\n" +
                 "B. 5,280\n" +
                 "C. 2,580\n" +
-                "D. 8,250");
+                "D. 8,250\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -414,11 +478,13 @@ namespace TriviaGame
         private int QuestionTen(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question ten: What is E.T.'s favorite candy?\n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question TEN: What is E.T.'s favorite candy?\n" +
                 "A. Reese's Pieces\n" +
                 "B. M&Ms\n" +
                 "C. Skittles\n" +
-                "D. Twizzler's");
+                "D. Twizzler's\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -448,11 +514,13 @@ namespace TriviaGame
         private int QuestionEleven(int score)
         {
             Console.Clear();
-            Console.WriteLine("Here is question eleven: Where is Elon Musk from? \n" +
+            Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                "Here is question ELEVEN: Where is Elon Musk from? \n" +
                 "A. South Africa\n" +
                 "B. England\n" +
                 "C. Australia\n" +
-                "D. United States");
+                "D. United States\n" +
+                "----------------------------------------------------------------------------------");
             string answer = Console.ReadLine().ToLower();
             switch (answer)
             {
@@ -483,11 +551,13 @@ namespace TriviaGame
         {
             {
                 Console.Clear();
-                Console.WriteLine("Here is question twelve: What was the name of the first electronic programmable computer?\n" +
+                Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                    "Here is question TWELVE: What was the name of the first electronic programmable computer?\n" +
                     "A. MAC\n" +
                     "B. IBM\n" +
                     "C. ENIAC \n" +
-                    "D. MSDOS");
+                    "D. MSDOS\n" +
+                    "----------------------------------------------------------------------------------");
                 string answer = Console.ReadLine().ToLower();
                 switch (answer)
                 {
@@ -519,11 +589,13 @@ namespace TriviaGame
         {
             {
                 Console.Clear();
-                Console.WriteLine("Here is question thirteen: Who invented Linux?\n" +
+                Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                    "Here is question THIRTEEN: Who invented Linux?\n" +
                     "A. Steve Jobs\n" +
                     "B. Linus Torvalds\n" +
                     "C. Bill Gates\n" +
-                    "D. Larry Page");
+                    "D. Larry Page\n" +
+                    "----------------------------------------------------------------------------------");
                 string answer = Console.ReadLine().ToLower();
                 switch (answer)
                 {
@@ -555,11 +627,13 @@ namespace TriviaGame
         {
             {
                 Console.Clear();
-                Console.WriteLine("Here is question fourteen: Who was credited with the invention of scissors?\n" +
+                Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                    "Here is question FOURTEEN: Who was credited with the invention of scissors?\n" +
                     "A. Nikola Tesla\n" +
                     "B. Benjamin Franklin\n" +
                     "C. John Rockefeller\n" +
-                    "D. Leonardo da Vinci");
+                    "D. Leonardo da Vinci\n" +
+                    "----------------------------------------------------------------------------------");
                 string answer = Console.ReadLine().ToLower();
                 switch (answer)
                 {
@@ -591,11 +665,13 @@ namespace TriviaGame
         {
             {
                 Console.Clear();
-                Console.WriteLine("Here is question fifteen: What year was the first portable laptop computer released to the public?\n" +
+                Console.WriteLine("----------------------------------------------------------------------------------\n" +
+                    "Final question! Here is question FIFTEEN: What year was the first portable laptop computer released to the public?\n" +
                     "A. 1996\n" +
                     "B. 1988\n" +
                     "C. 1993\n" +
-                    "D. 1981");
+                    "D. 1981\n" +
+                    "----------------------------------------------------------------------------------");
                 string answer = Console.ReadLine().ToLower();
                 switch (answer)
                 {
